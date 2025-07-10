4ec20a5c1b577701a9b7ecabd78f92c94010e607
Lordred1 <158574369+Lordred1@users.noreply.github.com>
2025-07-11 00:50:33 +0100

    s

--- gui/gui.cpp
+++ gui/gui.cpp
@@ -3,11 +3,16 @@
 
 Frame::Frame(const wxString& title): wxFrame(nullptr, wxID_ANY, title){
 	wxPanel* injectButtonPanel = new wxPanel(this);
-	wxButton* injectButton = new wxButton(injectButtonPanel, wxID_ANY, "Inject", wxPoint(150, 50) , wxSize(100,35));	
+	wxButton* injectButton = new wxButton(injectButtonPanel, wxID_ANY, "Inject", wxPoint(150, 50) , wxSize(100,35));
 	injectButton->Bind(wxEVT_BUTTON, &Frame::OnButtonClick, this);
+	CreateStatusBar();
 }
 
 void Frame::OnButtonClick(wxCommandEvent& evt){
-	wxExecute("./testfile.sh");
+	//wxExecute("./testfile.sh");
+    long exitCode = wxExecute("./testfile.sh", wxEXEC_SYNC);
+    if (exitCode != 0) {
+        wxLogStatus("FAILED TO INJECT");
+	}
 }
 
